0af6c1368a1011aa1fcc672bfc6c3c156a2b89c3
Hunter Stratton <hstratton16@gmail.com>
2023-05-26 22:32:30 -0600

    Handle user input

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,95 @@
-import { TicTacToe } from "./game";
+import { TicTacToe } from "./game/game";
 import * as readline from "readline";
-import { stdout, stdin } from "process";
+import { stdin } from "process";
+import { nextKeyPress, prompt } from "./prompt/prompt";
+import { Cell, Winner } from "./types";
 
-const reader = readline.createInterface(stdin, stdout);
+readline.emitKeypressEvents(stdin);
+stdin.setRawMode(true);
+
 const game = new TicTacToe();
+const selection = {
+  x: 0,
+  y: 0,
+};
+let message: string = "";
 
-console.clear();
+const handleInput = async (player: Cell) => {
+  const input = await nextKeyPress();
 
-console.log("Tic Tac Toe");
+  switch (input) {
+    case "d":
+    case "right":
+      selection.x += 1;
+      break;
+    case "a":
+    case "left":
+      selection.x -= 1;
+      break;
+    case "w":
+    case "up":
+      selection.y -= 1;
+      break;
+    case "s":
+    case "down":
+      selection.y += 1;
+      break;
+    case "space":
+    case "return":
+      try {
+        game.move(selection.x, selection.y);
+        message = "";
+      } catch (err) {
+        message = err.message;
+      }
+      break;
+    case "q":
+      process.exit();
+      break;
+  }
 
-game.render();
+  selection.x = selection.x >= 0 ? Math.min(selection.x, 2) : 0;
+  selection.y = selection.y >= 0 ? Math.min(selection.y, 2) : 0;
+};
 
-reader.question("Name: ", (answer) => {
-  console.log(answer);
-});
+const run = async () => {
+  const playersPiece = await prompt<Cell>("Would you like to play as X or O?", [
+    Cell.X,
+    Cell.O,
+  ]);
+
+  console.clear();
+
+  game.render();
+
+  while (!game.winner) {
+    if (game.currentTurn !== playersPiece) {
+      game.autoMove();
+    }
+
+    console.clear();
+
+    game.render(selection);
+
+    console.log(message);
+    console.log("Select a cell with Arrow Keys or WASD.");
+    console.log("Enter or Space to continue.");
+    console.log("Q to exit");
+
+    if (!game.winner) {
+      await handleInput(playersPiece);
+    }
+  }
+
+  console.clear();
+
+  if (game.winner === Winner.TIE) {
+    console.log(`\nIt was a tie!\n`);
+  } else {
+    console.log(`\n${game.winner} won the game!\n`);
+  }
+
+  process.exit();
+};
+
+run();
