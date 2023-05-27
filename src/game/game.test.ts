8cbbad00286e3c777f8ead4596590b54579fb6a6
Hunter Stratton <hstratton16@gmail.com>
2023-05-26 22:32:02 -0600

    Add auto move functionality

--- src/game/game.test.ts
+++ src/game/game.test.ts
@@ -1,4 +1,4 @@
-import { Cell } from "../types";
+import { Cell, Winner } from "../types";
 import { TicTacToe } from "./game";
 
 describe("game", () => {
@@ -23,14 +23,92 @@ describe("game", () => {
     expect(cellsDrawn.includes(Cell.X)).toBeFalsy();
   });
 
+  it("renders selection", async () => {
+    game.render({ x: 0, y: 0 });
+
+    const cellsDrawn = logSpy.mock.calls[0].slice(1);
+
+    expect(cellsDrawn.includes("#")).toBeTruthy();
+  });
+
+  it("renders selection when current position is taken", async () => {
+    game.move(0, 0);
+    game.render({ x: 0, y: 0 });
+
+    const cellsDrawn = logSpy.mock.calls[0].slice(1);
+
+    expect(cellsDrawn.includes("#")).toBeFalsy();
+    expect(cellsDrawn.includes("x")).toBeTruthy();
+  });
+
   it("throws when invalid move position", () => {
-    expect(() => game.move(-1, 0, Cell.X)).toThrow();
-    expect(() => game.move(0, -1, Cell.X)).toThrow();
-    expect(() => game.move(3, 0, Cell.X)).toThrow();
-    expect(() => game.move(0, 3, Cell.X)).toThrow();
+    expect(() => game.move(-1, 0)).toThrow();
+    expect(() => game.move(0, -1)).toThrow();
+    expect(() => game.move(3, 0)).toThrow();
+    expect(() => game.move(0, 3)).toThrow();
   });
 
   it("throws when position is taken", () => {
-    expect(() => game.move(0, 0, Cell.X).move(0, 0, Cell.O)).toThrow();
+    expect(() => game.move(0, 0).move(0, 0)).toThrow();
+  });
+
+  it("throws when game is already over", () => {
+    expect(() =>
+      game.move(0, 0).move(0, 1).move(1, 1).move(0, 2).move(2, 2).move(1, 0)
+    ).toThrow("Game already completed");
+  });
+
+  it("throws when index is out of bounds", () => {
+    expect(() => game.moveAtIndex(-1)).toThrow();
+    expect(() => game.moveAtIndex(9)).toThrow();
+  });
+
+  it("correctly finds row winner", () => {
+    // Setup moves for a top row win for X
+    game.move(0, 0).move(0, 1).move(1, 0).move(0, 2).move(2, 0);
+
+    expect(game.winner).toBe(Winner.X);
+  });
+
+  it("correctly finds column winner", () => {
+    // Setup moves for a second column win for O
+    game.move(0, 0).move(1, 0).move(0, 1).move(1, 1).move(2, 0).move(1, 2);
+
+    expect(game.winner).toBe(Winner.O);
+  });
+
+  it("correctly finds diagonal winner", () => {
+    // Setup moves for a diagonal X win
+    game.move(0, 0).move(1, 0).move(1, 1).move(2, 1).move(2, 2);
+
+    expect(game.winner).toBe(Winner.X);
+  });
+
+  it("correctly finds tie", () => {
+    [0, 1, 2, 4, 3, 5, 7, 6, 8].forEach((i) => game.moveAtIndex(i));
+    expect(game.winner).toBe(Winner.TIE);
+  });
+
+  it("makes winning move", () => {
+    // Should play winning move for X
+    game.move(0, 0).move(1, 1).move(0, 1).move(1, 0).autoMove();
+
+    expect(game.winner).toBe(Winner.X);
+  });
+
+  // Since ideal tic tac toe play results in a tie
+  it("should tie when only auto move is played", () => {
+    game
+      .autoMove()
+      .autoMove()
+      .autoMove()
+      .autoMove()
+      .autoMove()
+      .autoMove()
+      .autoMove()
+      .autoMove()
+      .autoMove();
+
+    expect(game.winner).toBe(Winner.TIE);
   });
 });
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,9 +1,29 @@
-import { Board, Cell } from "../types";
+import { Board, Cell, Winner } from "../types";
 
 export class TicTacToe {
-  board: Board = Array.from({ length: 9 }).map(() => Cell.EMPTY);
+  board: Board;
+  winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  winner: Winner;
+  currentTurn: Cell;
 
-  render = () => {
+  constructor(initialBoard?: Cell[], initialTurn?: Cell) {
+    this.board =
+      initialBoard ?? Array.from({ length: 9 }).map(() => Cell.EMPTY);
+    this.currentTurn = initialTurn ?? Cell.X;
+  }
+
+  render = (selection?: { x: number; y: number }) => {
+    const selectionIndex = selection ? selection.y * 3 + selection.x : -1;
+
     const output = `
    %s │ %s │ %s
   ───┼───┼───
@@ -12,23 +32,151 @@ export class TicTacToe {
    %s │ %s │ %s
     `;
 
-    console.log(output, ...this.board);
+    const cells = this.board.map((c, i) => {
+      if (i === selectionIndex) {
+        return c === Cell.EMPTY ? "#" : c.toLowerCase();
+      }
+
+      return c;
+    });
+
+    console.log(output, ...cells);
 
     return this;
   };
 
-  move = (x: number, y: number, player: Cell) => {
+  move = (x: number, y: number) => {
     if (x > 2 || x < 0 || y > 2 || y < 0) {
       throw new Error("Invalid position");
     }
-
     const index = y * 3 + x;
+
+    return this.moveAtIndex(index);
+  };
+
+  moveAtIndex = (index: number) => {
+    if (index < 0 || index > 8) {
+      throw new Error("Invalid position");
+    }
 
     if (this.board[index] !== Cell.EMPTY) {
       throw new Error("Position already taken");
     }
 
-    this.board[index] = player;
+    if (this.winner) {
+      throw new Error("Game already completed");
+    }
+
+    this.board[index] = this.currentTurn;
+
+    this.currentTurn = this.currentTurn === Cell.X ? Cell.O : Cell.X;
+
+    this.getWinner();
+
+    return this;
+  };
+
+  // Rank a move based on possible game outcomes.
+  // This was the first solution I came up with, I'm sure there is a clever dynamic approach that would be more efficient.
+  // Even this algorithm could be improved by aborting the search early when we find a winning move.
+  // Another approach could be to pre calculate all possible moves and simply look up the next best move in a hash table.
+  rankMove = (
+    game: TicTacToe,
+    desiredWinner: Winner,
+    index: number,
+    depth: number
+  ): number => {
+    game.moveAtIndex(index);
+
+    // We want to prioritize winning in fewer moves.
+    // A parent move should be worth as much as all of it's possible child moves.
+    const multiplier = 1 / (depth * 9 + 1);
+
+    if (game.winner === desiredWinner) {
+      return 2 * multiplier;
+    }
+
+    if (game.winner === Winner.TIE) {
+      return 1 * multiplier;
+    }
+
+    if (game.winner) {
+      return -2 * multiplier;
+    }
+
+    return game.board.reduce((prev, cell, i) => {
+      if (cell === Cell.EMPTY) {
+        const nextGame = new TicTacToe([...game.board], game.currentTurn);
+        return prev + nextGame.rankMove(nextGame, desiredWinner, i, depth + 1);
+      }
+      return prev;
+    }, 0);
+  };
+
+  // Plays the best move based on the current board
+  autoMove = () => {
+    // If this is the first move pick a random starting position for fun
+    if (this.board.every((c) => c === Cell.EMPTY)) {
+      return this.moveAtIndex(Math.floor(Math.random() * 9));
+    }
+
+    // Create a list of possible moves sorted by their ranking.
+    const moves = this.board
+      .map((c, i) => ({
+        cell: c,
+        index: i,
+      }))
+      .filter(({ cell }) => cell === Cell.EMPTY)
+      .map(({ index, cell }) => {
+        const nextGame = new TicTacToe([...this.board], this.currentTurn);
+        const rank = nextGame.rankMove(
+          nextGame,
+          this.currentTurn === Cell.O ? Winner.O : Winner.X,
+          index,
+          0
+        );
+
+        return {
+          rank,
+          index,
+          cell,
+        };
+      })
+      .sort((a, b) => b.rank - a.rank);
+
+    // Make the highest ranked move
+    const moveIndex = moves[0].index;
+    return this.moveAtIndex(moveIndex);
+  };
+
+  getWinner = () => {
+    // Return early if we already have a winner for this game.
+    if (this.winner) {
+      return this;
+    }
+
+    this.winningPositions.find((indices) => {
+      // Check if all indices in the given position are filled by the same cell type
+      const potentialWinner = indices
+        .slice(1)
+        .reduce(
+          (prev, current) =>
+            this.board[current] === prev ? this.board[current] : Cell.EMPTY,
+          this.board[indices[0]]
+        );
+
+      const playerWon = potentialWinner !== Cell.EMPTY;
+
+      if (playerWon) {
+        this.winner = potentialWinner === Cell.X ? Winner.X : Winner.O;
+      }
+
+      return playerWon;
+    });
+
+    if (!this.board.includes(Cell.EMPTY)) {
+      this.winner = Winner.TIE;
+    }
 
     return this;
   };
